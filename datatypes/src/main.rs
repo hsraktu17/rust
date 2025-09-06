ee263c9f01866d0060ad7a959224905813f8ae66
Utkarsh Raj Srivastava <utkarshrajsrivastava7@gmail.com>
2025-09-06 12:05:43 +0530

    updated datatypes

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -1,12 +1,16 @@
 fn main() {
-    
-    let a :i32 = 49;
-    
-    let pi :f64 = 3.14159;
-    
+    let a: i32 = 49;
+
+    let pi: f64 = 3.14159;
+
     let s1 = String::from("hello");
-    
+
+    let mut ans = (a as f64) * pi;
+    ans += 1.10199;
+
+    println!("This is checking for mutable {}", 156.0 - ans);
+
     println!("{}", s1);
-    println!("{}", (a as f64)*pi);
+    println!("{}", (a as f64) * pi);
     println!("Datatypes....");
 }
